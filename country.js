3b758ff7563bcfeb45ae5f8ef9e2ae990a069e82
Tommy Waalkes <Tommy@grandcircus.co>
2018-11-12 11:32:42 -0500

    Linked in Country.cs

---
+++ country.js
@@ -0,0 +1,18 @@
+class Country{
+    constructor(name, lang, greeting, colors){
+        this.name = name;
+        this.lang = lang;
+        this.greeting = greeting;
+        this.colors = colors;
+    }
+    setColors(){
+        
+    }
+}
+
+
+
+function SwitchCountry(){
+    let input = prompt("Please input a country");
+    
+}
