3e789fa5728aac1cd50f59863b8279308fb6b263
Zach Buth <zachbuth@gmail.com>
2023-08-21 16:46:00 -0400

    1st

--- country.js
+++ country.js
@@ -8,11 +8,13 @@ class Country {
 }
 let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
-
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"]);
+let italy = new Country("Italy", "Italian", "Ciao mondo", ["green", "white", "red"]);
+let germany = new Country("Germany", "German", "Hallo Welt", ["black","red", "gold"])
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = document.querySelector("#CountryList").value;
+  
     let country;
 
     if (input === "USA") {
@@ -25,5 +27,28 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
-
+    else if (input === "Italy") {
+        country = italy;
+    }
+    else if (input === "Germany") {
+        country = germany;
+    }
+    DisplayColors(country);
 }
+
+function DisplayColors(c){
+    let nameNode = document.querySelector("#CountryName")   
+    nameNode.innerText = c.name;
+    let langNode = document.querySelector("#OfficialLanguage")   
+    langNode.innerText = c.lang;
+    let greetingNode = document.querySelector("#HelloWorld")   
+    greetingNode.innerText = c.greeting;
+    document.querySelector("#color1").style.backgroundColor = c.colors[0];
+    document.querySelector("#color2").style.backgroundColor = c.colors[1];
+    document.querySelector("#color3").style.backgroundColor = c.colors[2];
+   
+}
+
+
+
+
